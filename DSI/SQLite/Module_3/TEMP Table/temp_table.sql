bb171c58d369dbf857f675ead591a9ddcdc5dd54
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-10 03:13:44 +0800

    Update temp_table.sql

--- DSI/SQLite/Module_3/TEMP Table/temp_table.sql
+++ DSI/SQLite/Module_3/TEMP Table/temp_table.sql
@@ -1,39 +1,23 @@
--- HAVING
+-- temp tables
 
--- how much did a customer spend on each day
-SELECT
-market_date,
-customer_id,
-SUM(quantity*cost_to_customer_per_qty) as total_cost
-FROM customer_purchases -- first
-WHERE customer_id BETWEEN 1 AND 5 -- filtering the non-aggregated values -- second
-GROUP BY market_date, customer_id -- third
-HAVING total_cost > 50; -- filtering the aggregated values -- fourth
+-- if a table named new_vendor_inventory exists, delete it, other do NOTHING 
+DROP TABLE IF EXISTS temp.new_vendor_inventory;
 
--- how many product were bought?
+-- make the table 
+CREATE TABLE temp.new_vendor_inventory AS
 
-SELECT
-COUNT(product_id) AS number_of_products,
-product_id
+-- definition of the table 
+SELECT *,
+original_price*5 as inflation
+FROM vendor_inventory;
 
-FROM customer_purchases
-WHERE product_id <= 8
-GROUP BY product_id
-HAVING count(product_id) BETWEEN 300 AND 500
+SELECT * FROM temp.new_vendor_inventory
 
+-- put the table into another temp TABLE
 
-SELECT
-COUNT(product_id) AS number_of_products,
-product_id
-
-FROM customer_purchases
-WHERE product_id <= 8
-GROUP BY product_id
-HAVING number_of_products BETWEEN 300 AND 500
-
-
-
-
-
-
-
+DROP TABLE IF EXISTS temp.new_new_vendor_inventory;
+CREATE TABLE temp.new_new_vendor_inventory AS
+SELECT *,
+inflation*2 AS super_inflantion
+FROM temp.new_vendor_inventory;
+SELECT * FROM new_new_vendor_inventory
