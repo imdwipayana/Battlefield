7c9af57af750259fd020703c2f297d75cc4813cb
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-10 01:32:18 +0800

    Update having.sql

--- DSI/SQLite/Module_3/HAVING/having.sql
+++ DSI/SQLite/Module_3/HAVING/having.sql
@@ -1,34 +1,35 @@
--- count
--- count the number of products
+-- HAVING
 
-SELECT COUNT(product_id) AS num_of_product
-FROM product;
+-- how much did a customer spend on each day
+SELECT
+market_date,
+customer_id,
+SUM(quantity*cost_to_customer_per_qty) as total_cost
+FROM customer_purchases -- first
+WHERE customer_id BETWEEN 1 AND 5 -- filtering the non-aggregated values -- second
+GROUP BY market_date, customer_id -- third
+HAVING total_cost > 50; -- filtering the aggregated values -- fourth
 
--- how many products per product_qty_type
-SELECT 
-product_qty_type,
-COUNT(product_id) AS num_of_products
-FROM product
-GROUP BY product_qty_type
-
--- how many products per product_qty_type and per their product_size
+-- how many product were bought?
 
 SELECT
-product_size,
-product_qty_type,
-COUNT(product_id) AS num_of_products
-FROM product
-GROUP BY product_size, product_qty_type;
+COUNT(product_id) AS number_of_products,
+product_id
 
-
--- count DISTINCT
--- how many unique products were bought
-
-SELECT COUNT(DISTINCT product_id) as bought_product
 FROM customer_purchases
+WHERE product_id <= 8
+GROUP BY product_id
+HAVING count(product_id) BETWEEN 300 AND 500
 
 
+SELECT
+COUNT(product_id) AS number_of_products,
+product_id
 
+FROM customer_purchases
+WHERE product_id <= 8
+GROUP BY product_id
+HAVING number_of_products BETWEEN 300 AND 500
 
 
 
