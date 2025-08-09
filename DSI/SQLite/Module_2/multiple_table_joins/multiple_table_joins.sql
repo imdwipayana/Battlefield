77f2c337b08f353b4e4b497eaa73f81616de92a5
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-10 00:23:39 +0800

    Update multiple_table_joins.sql

--- DSI/SQLite/Module_2/multiple_table_joins/multiple_table_joins.sql
+++ DSI/SQLite/Module_2/multiple_table_joins/multiple_table_joins.sql
@@ -1,29 +1,35 @@
--- WHERE
+-- multiple table joins
 
-SELECT * FROM customer
-WHERE customer_id = 1
-OR customer_id =2;
+/* Which vendor has sold products to a customer
+... and which product was it?
+.. and to whom was it sold*/
 
--- IN
-SELECT * FROM customer
-WHERE customer_id IN (3,4,5)
-OR customer_postal_code IN ('M4H', 'M1L');
+SELECT DISTINCT
+c.customer_first_name,
+c.customer_last_name,
+v.vendor_name,
+p.product_name
+FROM customer_purchases as cp
+INNER JOIN customer as c
+ON cp.customer_id = c.customer_id
+INNER JOIN vendor as v
+ON v.vendor_id = cp.vendor_id
+INNER JOIN product as p
+ON p.product_id = cp.product_id;
 
--- like
--- all the peppers
-SELECT * FROM product
-WHERE product_name LIKE '%pepper%';
+/* what if we add the dates they were purchased? */
+SELECT DISTINCT
+cp.market_date,
+c.customer_first_name,
+c.customer_last_name,
+v.vendor_name,
+p.product_name
 
--- customer with a last name starting with a
-SELECT * FROM customer
-WHERE customer_last_name LIKE 'a%';
+FROM customer_purchases as cp
+INNER JOIN customer as c
+ON cp.customer_id = c.customer_id
+INNER JOIN vendor as v
+ON v.vendor_id = cp.vendor_id
+INNER JOIN product as p
+ON p.product_id = cp.product_id;
 
--- NULLS and Blanks
-SELECT * FROM product
-WHERE product_size IS NULL
-OR product_size = ''; --two single quotes, "blanks" different from nulls
-
--- between another option
-SELECT *
-FROM customer
-WHERE customer_id BETWEEN 1 AND 20
