e8ac6ee10fe4ed4aadfc1b0de1ccd5b9ba5bf8f4
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-10 00:32:32 +0800

    Update count.sql

--- DSI/SQLite/Module_3/COUNT/count.sql
+++ DSI/SQLite/Module_3/COUNT/count.sql
@@ -1,48 +1,35 @@
---CASE
+-- count
+-- count the number of products
 
--- add some logic to determine which vendors come on which days
+SELECT COUNT(product_id) AS num_of_product
+FROM product;
+
+-- how many products per product_qty_type
+SELECT 
+product_qty_type,
+COUNT(product_id) AS num_of_products
+FROM product
+GROUP BY product_qty_type
+
+-- how many products per product_qty_type and per their product_size
+
 SELECT
-	*,
-	CASE
-		WHEN vendor_type = 'Fresh Focused' THEN 'Wednesday'
-		WHEN vendor_type = 'Prepared Foods' THEN 'Thursday'
-		ELSE 'Saturday'
-	END AS day_of_specialty,
-	/*-- pie day, otherwise nothing*/
-	
-	CASE 
-		WHEN vendor_name = "Annie's Pies" THEN 'annie is the best' --- double quotes will work just this once!
-	END AS annie_is_the_king,
-	
-	CASE 
-		WHEN vendor_name LIKE '%pie%' THEN 'Wednesday'
-		ELSE 'Friday' -- with the else, we get values for FALSE statements
-	END AS pie_day,
-	-- nonsense, but not a string, instead a different COLUMN
-	
-	CASE
-		WHEN vendor_type = 'Freash Focused' THEN vendor_owner_first_name
-		WHEN vendor_type = 'Prepared Foods' THEN vendor_owner_last_name
-	END AS first_or_last
-FROM vendor
-	
-	
-	 
+product_size,
+product_qty_type,
+COUNT(product_id) AS num_of_products
+FROM product
+GROUP BY product_size, product_qty_type;
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+-- count DISTINCT
+-- how many unique products were bought
+
+SELECT COUNT(DISTINCT product_id) as bought_product
+FROM customer_purchases
+
+
+
+
 
 
 
