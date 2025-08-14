6468a3fff5b4c1d946398365b15ef7a71d94cc59
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-14 00:33:01 -0400

    Update spotify.sql

--- SQL/PostgreSQL/Project 1 Spotify/spotify.sql
+++ SQL/PostgreSQL/Project 1 Spotify/spotify.sql
@@ -30,15 +30,102 @@ CREATE TABLE spotify (
 );
 
 SELECT
-* 
+COUNT(*) 
 FROM spotify
 
+/*
+Retrieve the names of all tracks that have more than 1 billion streams.
+List all albums along with their respective artists.
+Get the total number of comments for tracks where licensed = TRUE.
+Find all tracks that belong to the album type single.
+Count the total number of tracks by each artist.
+*/
+
+--===========================================================================
+-- 1. Retrieve the names of all tracks that have more than 1 billion streams.
+--===========================================================================
+SELECT
+	track,
+	stream
+FROM spotify
+WHERE stream > 1000000000
+
+--===========================================================================
+-- 2. List all albums along with their respective artists.
+--===========================================================================
+SELECT
+	DISTINCT album,
+	artist
+FROM spotify
+
+-- Detect row duplicate
+SELECT
+	album,
+	COUNT(album)
+FROM spotify
+GROUP BY album
+
+-- Calling data contains row duplicate
+SELECT
+	*
+FROM spotify
+WHERE album = 'Acústico (Ao Vivo / Deluxe)'
+
+--===========================================================================
+-- 3. Get the total number of comments for tracks where licensed = TRUE.
+--===========================================================================
+SELECT
+	SUM(comments)
+FROM spotify
+WHERE licensed = 'true'
+
+--===========================================================================
+-- 4. Find all tracks that belong to the album type single.
+--===========================================================================
+SELECT
+	track
+FROM spotify
+WHERE album_type = 'single'
+
+--===========================================================================
+-- 5. Count the total number of tracks by each artist.
+--===========================================================================
+SELECT
+	artist,
+	COUNT(track)
+FROM spotify
+GROUP BY artist
+
+/*
+Calculate the average danceability of tracks in each album.
+Find the top 5 tracks with the highest energy values.
+List all tracks along with their views and likes where official_video = TRUE.
+For each album, calculate the total views of all associated tracks.
+Retrieve the track names that have been streamed on Spotify more than YouTube.
+*/
+--===========================================================================
+-- 6. Calculate the average danceability of tracks in each album.
+--===========================================================================
 
 
+--===========================================================================
+-- 7. Find the top 5 tracks with the highest energy values.
+--===========================================================================
 
 
+--===========================================================================
+-- 8. List all tracks along with their views and likes where official_video = TRUE.
+--===========================================================================
 
 
+--===========================================================================
+-- 9. For each album, calculate the total views of all associated tracks.
+--===========================================================================
+
+
+--===========================================================================
+-- 10. Retrieve the track names that have been streamed on Spotify more than YouTube.
+--===========================================================================
 
 
 
