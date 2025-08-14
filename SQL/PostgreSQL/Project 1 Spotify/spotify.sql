89c0ee571b997ef273e02764babb5dba2db636ba
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-14 00:43:51 -0400

    Update spotify.sql

--- SQL/PostgreSQL/Project 1 Spotify/spotify.sql
+++ SQL/PostgreSQL/Project 1 Spotify/spotify.sql
@@ -106,26 +106,63 @@ Retrieve the track names that have been streamed on Spotify more than YouTube.
 --===========================================================================
 -- 6. Calculate the average danceability of tracks in each album.
 --===========================================================================
-
+SElECT
+	album,
+	AVG(danceability) as average_danceability
+FROM spotify
+GROUP by album
 
 --===========================================================================
 -- 7. Find the top 5 tracks with the highest energy values.
 --===========================================================================
+SELECT
+	track,
+	energy
+FROM spotify
+ORDER BY energy DESC
+LIMIT 5
 
-
---===========================================================================
+--=================================================================================
 -- 8. List all tracks along with their views and likes where official_video = TRUE.
---===========================================================================
-
+--=================================================================================
+SELECT
+	track,
+	views,
+	likes
+FROM spotify
+WHERE official_video = 'true'
 
 --===========================================================================
 -- 9. For each album, calculate the total views of all associated tracks.
 --===========================================================================
+SELECT
+	album,
+	SUM(views) as total_views
+FROM spotify
+GROUP BY album
 
-
---===========================================================================
+--===================================================================================
 -- 10. Retrieve the track names that have been streamed on Spotify more than YouTube.
---===========================================================================
+--===================================================================================
+SELECT
+	track
+FROM spotify
+WHERE most_played_on = 'Spotify'
 
+/*
+Find the top 3 most-viewed tracks for each artist using window functions.
+Write a query to find tracks where the liveness score is above the average.
+Use a WITH clause to calculate the difference between the highest and lowest energy values for tracks in each album.
+*/
+--===================================================================================
+-- 11. Find the top 3 most-viewed tracks for each artist using window functions.
+--===================================================================================
 
+--===================================================================================
+-- 12. Write a query to find tracks where the liveness score is above the average.
+--===================================================================================
+
+--===================================================================================
+-- 13. Use a WITH clause to calculate the difference between the highest and lowest energy values for tracks in each album.
+--===================================================================================
 
