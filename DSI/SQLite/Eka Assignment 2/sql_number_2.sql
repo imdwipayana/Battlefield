cda67b754fa65b56e8249c0a8d5e7bfe9a57d2e7
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-14 09:43:03 -0400

    Update sql_number_2.sql

--- DSI/SQLite/Eka Assignment 2/sql_number_2.sql
+++ DSI/SQLite/Eka Assignment 2/sql_number_2.sql
@@ -602,5 +602,269 @@ FROM
 CROSS JOIN
     colors_trial AS c;
 
---=========================================
+--=============================================================================================================================
+-- Checking one by one
+--=============================================================================================================================
+-- COALESCE
+/* 1. Our favourite manager wants a detailed long list of products, but is afraid of tables! 
+We tell them, no problem! We can produce a list with all of the appropriate details. 
+
+Using the following syntax you create our super cool and not at all needy manager a list:
+
+SELECT 
+product_name || ', ' || product_size|| ' (' || product_qty_type || ')'
+FROM product
+
+But wait! The product table has some bad data (a few NULL values). 
+Find the NULLs and then using COALESCE, replace the NULL with a 
+blank for the first problem, and 'unit' for the second problem. 
+
+HINT: keep the syntax the same, but edited the correct components with the string. 
+The `||` values concatenate the columns into strings. 
+Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
+All the other rows will remain the same.) */
+
+WITH CTE_no_null AS (
+	SELECT
+		*,
+		COALESCE(product_size, '') as product_size_nonull,
+		COALESCE(product_qty_type, 'unit') as product_qty_type_nonull
+	FROM product
+)
+SELECT 
+	product_name || ', ' || product_size_nonull|| ' (' || product_qty_type_nonull || ')'
+FROM CTE_no_null;
+
+
+--=============================================================================================================================
+-- Checking one by one
+--=============================================================================================================================
+
+
+--Windowed Functions
+/* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
+visits to the farmer’s market (labeling each market date with a different number). 
+Each customer’s first visit is labeled 1, second visit is labeled 2, etc. 
+
+You can either display all rows in the customer_purchases table, with the counter changing on
+each new market date for each customer, or select only the unique market dates per customer 
+(without purchase details) and number those visits. 
+HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
+
+WITH CTE_visit_number AS(
+	SELECT 
+		customer_id,
+		market_date,
+		DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY market_date) as visit_number
+	FROM customer_purchases
+)
+
+SELECT 
+	DISTINCT visit_number,
+	customer_id,
+	market_date
+FROM CTE_visit_number;
+
+/* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
+then write another query that uses this one as a subquery (or temp table) and filters the results to 
+only the customer’s most recent visit. */
+
+WITH CTE_visit_number AS(
+	SELECT 
+		customer_id,
+		market_date,
+		DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY market_date DESC) as visit_number
+	FROM customer_purchases
+)
+
+SELECT
+	DISTINCT customer_id,
+	market_date AS recent_visit
+FROM CTE_visit_number
+WHERE visit_number = 1;
+
+
+/* 3. Using a COUNT() window function, include a value along with each row of the 
+customer_purchases table that indicates how many different times that customer has purchased that product_id. */
+
+WITH  CTE_product AS (
+	SELECT
+		customer_id,
+		product_id,
+		COUNT() OVER(PARTITION BY customer_id, product_id) as product_bought_number
+	FROM customer_purchases
+)
+SELECT DISTINCT
+	customer_id,
+	product_id,
+	product_bought_number
+FROM CTE_product;
+
+
+DROP TABLE IF EXISTS trial_table;
+CREATE TABLE trial_table(
+number_1 INT,
+number_2 INT,
+number_3 INT,
+number_4 INT)
+
+INSERT INTO trial_table
+VALUES 
+(1,2,3,4),
+(1,2,3,4),
+(1,2,3,5),
+(1,2,4,5),
+(2,2,3,4);
+
+SELECT DISTINCT
+number_2
+
+FROM trial_table
+
+SELECT
+*
+FROM trial_table
+
+--=============================================================================================================================
+-- Checking one by one
+--=============================================================================================================================
+-- String manipulations
+/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
+These are separated from the product name with a hyphen. 
+Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
+Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
+
+| product_name               | description |
+|----------------------------|-------------|
+| Habanero Peppers - Organic | Organic     |
+
+Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
+
+WITH CTE_hypen AS (
+SELECT 
+	*,
+  TRIM(SUBSTR(product_name, 1, INSTR(product_name, '- ') - 1)) AS product_name_clear
+FROM product
+)
+SELECT
+	product_name AS full_product_name,
+	CASE
+		WHEN LENGTH(product_name_clear) > 0 THEN product_name_clear
+		ELSE product_name
+	END AS product_name,
+
+	CASE
+		WHEN LENGTH(product_name_clear) > 0 THEN TRIM(SUBSTR(product_name, INSTR(product_name, '- ') + 2), -1)
+		ELSE ''
+	END AS description
+FROM CTE_hypen;
+
+/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
+
+SELECT 
+	*
+FROM product 
+WHERE product_size REGEXP '\d';
+
+
+--=============================================================================================================================
+-- Checking one by one
+--=============================================================================================================================
+
+
+-- UNION
+/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
+
+HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
+1) Create a CTE/Temp Table to find sales values grouped dates; 
+2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
+"best day" and "worst day"; 
+3) Query the second temp table twice, once for the best day, once for the worst day, 
+with a UNION binding them. */
+
+WITH CTE_purchase AS (
+	SELECT
+		*,
+		quantity * cost_to_customer_per_qty as purchase_each
+	FROM customer_purchases
+), CTE_daily_sales AS (
+	SELECT DISTINCT
+		market_date,
+		SUM(purchase_each) OVER(PARTITION BY market_date) as daily_sales
+	FROM CTE_purchase
+), CTE_sales_minimum AS (
+	SELECT
+		*
+	FROM CTE_daily_sales
+	ORDER BY daily_sales 
+	LIMIT 1
+), CTE_sales_maximum AS (
+	SELECT
+		*
+	FROM CTE_daily_sales
+	ORDER BY daily_sales DESC
+	LIMIT 1
+)
+
+SELECT
+	*
+FROM CTE_sales_minimum
+
+UNION
+
+SELECT
+	*
+FROM CTE_sales_maximum;
+
+--=============================================================================================================================
+-- Checking one by one
+--=============================================================================================================================
+
+-- Cross Join
+/*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
+customer on record. How much money would each vendor make per product? 
+Show this by vendor_name and product name, rather than using the IDs.
+
+HINT: Be sure you select only relevant columns and rows. 
+Remember, CROSS JOIN will explode your table rows, so CROSS JOIN should likely be a subquery. 
+Think a bit about the row counts: how many distinct vendors, product names are there (x)?
+How many customers are there (y). 
+Before your final group by you should have the product of those two queries (x*y).  */
+
+WITH CTE_vendor AS (
+	SELECT
+		vendor_id,
+		product_id,
+		original_price
+	FROM vendor_inventory
+), CTE_customer AS (
+	SELECT
+		customer_id
+	FROM customer
+), CTE_cross_join AS (
+	SELECT
+		cv.vendor_id,
+		cv.product_id,
+		cv.original_price,
+		cc.customer_id
+	FROM CTE_vendor AS cv
+	CROSS JOIN CTE_customer AS cc
+)
+
+SELECT DISTINCT 
+	product_id,
+	vendor_id,
+	SUM(original_price) OVER(PARTITION BY product_id ORDER BY vendor_id) AS sales_each_product
+FROM CTE_cross_join
+
+
+
+
+
+
+
+
+
+
+
 
