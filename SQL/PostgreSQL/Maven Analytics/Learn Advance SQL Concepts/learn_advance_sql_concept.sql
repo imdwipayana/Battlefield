c49c91a72bf7bfa8222d77e146665e865bfd0b1a
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-16 06:17:27 -0400

    Update learn_advance_sql_concept.sql

--- SQL/PostgreSQL/Maven Analytics/Learn Advance SQL Concepts/learn_advance_sql_concept.sql
+++ SQL/PostgreSQL/Maven Analytics/Learn Advance SQL Concepts/learn_advance_sql_concept.sql
@@ -154,6 +154,7 @@ ORDER BY cd.sales_date;
 --=====================================================================
 -- 8. Use Window Function to impute the NULL values
 --=====================================================================
+-- First method:
 WITH CTE_recursive AS (
 WITH RECURSIVE CTE_date AS (
 SELECT CAST('2025-01-01' AS DATE) AS sales_date
@@ -179,5 +180,33 @@ ORDER BY cd.sales_date
 SELECT
 	*,
 	COALESCE(number_sales,AVG(number_sales) OVER(ORDER BY sales_date ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING)) AS average_sales_null
+FROM CTE_recursive;
+
+-- Second method:
+WITH CTE_recursive AS (
+WITH RECURSIVE CTE_date AS (
+SELECT CAST('2025-01-01' AS DATE) AS sales_date
+UNION ALL
+SELECT sales_date + 1
+FROM CTE_date
+WHERE sales_date < CAST('2025-01-07' AS DATE)
+), CTE_average AS (
+SELECT
+AVG(number_sales) AS average_sales
+FROM sales_data
+)
+
+SELECT
+	cd.sales_date,
+	sd.number_sales
+FROM CTE_date AS cd
+LEFT JOIN sales_data as sd
+ON sd.sales_date = cd.sales_date
+
+ORDER BY cd.sales_date
+)
+SELECT
+	*,
+	COALESCE(number_sales,ROUND((LAG(number_sales) OVER() +LEAD(number_sales) OVER())/2)) AS average_sales_null
 FROM CTE_recursive;
 
