0af0ddcf41a7d0a81e93d1bff61bbfc579bd9ddd
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-16 10:01:43 -0400

    Update restaurant.sql

--- SQL/MySQL/Maven Analytic/Restaurant Order/restaurant.sql
+++ SQL/MySQL/Maven Analytic/Restaurant Order/restaurant.sql
@@ -1,10 +1,34 @@
-SELECT
-ï»¿menu_item_id
-FROM restaurant.menu_items
-
+#=============================================================
+# Insert table using "Table Data Import Wizard", then
+# change the column name using the following syntax:
+#=============================================================
 ALTER TABLE restaurant.menu_items
 CHANGE ï»¿menu_item_id menu_item_id VARCHAR(100) NOT NULL;
 
+#=============================================================
+# Call all the tables
+#=============================================================
 SELECT 
-*
-FROM restaurant.menu_items
+	menu_item_id,
+	item_name,
+	category,
+	price
+FROM restaurant.menu_items;
+#=============================================================
+SELECT 
+order_details_id,
+order_id,
+order_date,
+order_time,
+item_id
+FROM restaurant.order_details;
+#==============================================================
+SELECT
+	table_details,
+	field,
+	description_column
+FROM restaurant.restaurant_db_data_dictionary
+
+
+
+
