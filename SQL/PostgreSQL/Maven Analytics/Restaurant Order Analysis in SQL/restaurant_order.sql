ef0eff0059064307064dcef3648b2545416f9f3e
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-16 09:26:53 -0400

    Update restaurant_order.sql

--- SQL/PostgreSQL/Maven Analytics/Restaurant Order Analysis in SQL/restaurant_order.sql
+++ SQL/PostgreSQL/Maven Analytics/Restaurant Order Analysis in SQL/restaurant_order.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS menu_items;
 CREATE TABLE menu_items (
-menu_item_id VARCHAR(25),
+menu_item_id INTEGER,
 item_name VARCHAR(100),
 category VARCHAR(50),
 price FLOAT
@@ -8,16 +8,40 @@ price FLOAT
 
 DROP TABLE IF EXISTS restaurant_db_data_dictionary;
 CREATE TABLE restaurant_db_data_dictionary (
-Table VARCHAR(50),
-Field VARCHAR(100),
-Description VARCHAR(250)
+table_detail VARCHAR(50),
+field VARCHAR(100),
+description VARCHAR(250)
 );
 
 DROP TABLE IF EXISTS order_details;
 CREATE TABLE order_details (
-order_details_id VARCHAR(10),
-order_id VARCHAR(10),
+order_details_id INTEGER,
+order_id INTEGER,
 order_date DATE,
 order_time TIME,
-item_id VARCHAR(10)
+item_id INTEGER 
 )
+--================================================================================================
+-- Calling the restaurant_db_data_dictionary table
+--================================================================================================
+SELECT 
+	*
+FROM restaurant_db_data_dictionary
+--================================================================================================
+-- Calling the menu_items table
+--================================================================================================
+SELECT 
+	*
+FROM menu_items
+--================================================================================================
+-- Calling the order_details table
+--================================================================================================
+SELECT 
+	*
+FROM order_details
+
+--================================================================================================
+-- Calling the order_details table
+--================================================================================================
+
+
