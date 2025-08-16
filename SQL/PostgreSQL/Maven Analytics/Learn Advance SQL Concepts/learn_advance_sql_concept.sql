01fec2b6492eddcce4bfc11aa1db11a5c4d360d3
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2025-08-15 23:08:32 -0400

    Update learn_advance_sql_concept.sql

--- SQL/PostgreSQL/Maven Analytics/Learn Advance SQL Concepts/learn_advance_sql_concept.sql
+++ SQL/PostgreSQL/Maven Analytics/Learn Advance SQL Concepts/learn_advance_sql_concept.sql
@@ -12,115 +12,36 @@ VALUES
 ('2025-01-05',  95),
 ('2025-01-07',  77);
 
+DROP TABLE IF EXISTS final_sales;
+CREATE TABLE final_sales (
+sales_date DATE,
+number_sales FLOAT
+);
 
-SELECT * FROM sales_data;
---===================================================================
--- List all managers with all of their subordinates. 
---===================================================================
--- First method:
-SELECT
-	*
-FROM employee_hierarchy
-WHERE employee_id = 'E001' OR manager_id = 'E001';
+INSERT INTO final_sales
+VALUES 
+('2025-01-01',  61),
+('2025-01-02',  72),
+('2025-01-03',  78),
+('2025-01-04',  84),
+('2025-01-05',  95),
+('2025-01-06',  86),
+('2025-01-07',  77);
 
-SELECT
-	*
-FROM employee_hierarchy
-WHERE employee_id = 'E002' OR manager_id = 'E002';
-
-SELECT
-	*
-FROM employee_hierarchy
-WHERE employee_id = 'E003' OR manager_id = 'E003';
-
-SELECT
-	*
-FROM employee_hierarchy
-WHERE employee_id = 'E004' OR manager_id = 'E004';
-
--- Second method: using recursive CTE
-WITH RECURSIVE REC_CTE_employee AS (
-SELECT
-	employee_id,
-	full_name,
-	manager_id
-FROM employee_hierarchy
-WHERE employee_id = 'E001'
-UNION
-SELECT
-	eh.employee_id,
-	eh.full_name,
-	eh.manager_id
-FROM employee_hierarchy AS eh
-INNER JOIN REC_CTE_employee AS rce
-ON eh.manager_id = rce.employee_id
-)
-SELECT *
-FROM REC_CTE_employee;
-
--- For the employee with employee_id = E1002
-WITH RECURSIVE REC_CTE_employee AS (
-SELECT
-	employee_id,
-	full_name,
-	manager_id
-FROM employee_hierarchy
-WHERE employee_id = 'E002'
-UNION
-SELECT
-	eh.employee_id,
-	eh.full_name,
-	eh.manager_id
-FROM employee_hierarchy AS eh
-INNER JOIN REC_CTE_employee AS rce
-ON eh.manager_id = rce.employee_id
-)
-SELECT *
-FROM REC_CTE_employee;
-
---NOTE: with a lot of repetition, it is interesting to use SQL function here.
-
---===========================================================================
--- 2. 
---===========================================================================
-WITH RECURSIVE REC_CTE_detect_null AS (
-SELECT
-	'E1001' AS order_id,
-	1 AS n
-UNION ALL
-SELECT 
-	concat('E100', n+1),
-	n+1
-FROM REC_CTE_detect_null
-WHERE n < 13
-)
-
+--=====================================================================
+-- 1. Call the sales_data table and look at the gap on date.
+--=====================================================================
 SELECT 
 	* 
-FROM REC_CTE_detect_null as rcdn
-LEFT JOIN employee_hierarchy as eh
-ON rcdn.order_id = eh.employee_id
-WHERE eh.full_name IS NULL;
+FROM sales_data;
 
-SELECT
-	*
-FROM employee_hierarchy
-WHERE full_name IS NULL
---------------------------------------------------------
-SELECT
-*
-FROM employee_hierarchy
-ORDER BY full_name
-LIMIT 10
+--=====================================================================
+-- 2. The final table of sales_data table must be like this:
+--=====================================================================
+SELECT 
+	* 
+FROM final_sales;
 
-SELECT
-*
-FROM(
-SELECT
-*
-FROM employee_hierarchy
-LIMIT 10
-)
-ORDER BY full_name
+
 
 
